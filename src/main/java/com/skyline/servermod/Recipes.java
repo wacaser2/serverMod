ea702744c1470dd573a9a391d4826b7dc4d22bc9
captainjolt <captainjolt@skylineacademy.org>
2020-11-10 00:03:41 -0600

    Update Recipes.java

--- src/main/java/com/skyline/servermod/Recipes.java
+++ src/main/java/com/skyline/servermod/Recipes.java
@@ -2,9 +2,15 @@ package com.skyline.servermod;
 
 import java.util.function.Consumer;
 
+import com.skyline.servermod.common.blocks.ModBlocks;
+import com.skyline.servermod.common.blocks.ModBlocks.BlockSet;
+
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 public class Recipes extends RecipeProvider implements IConditionBuilder {
@@ -13,10 +19,10 @@ public class Recipes extends RecipeProvider implements IConditionBuilder {
 		}
 
 		protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
-//			
-//			for(BlockSet blockSet : ModBlocks.VARIANT_SETS) {
-//				registerBlockSetRecipes(consumer, blockSet);
-//			}
+			
+			for(BlockSet blockSet : ModBlocks.VARIANT_SETS) {
+				registerBlockSetRecipes(consumer, blockSet);
+			}
 //			
 //			
 //			ResourceLocation ID = new ResourceLocation("data_gen_test", "conditional");
@@ -41,9 +47,32 @@ public class Recipes extends RecipeProvider implements IConditionBuilder {
 //					.build(consumer, ID);
 		}
 		
-//		private void registerBlockSetRecipes(Consumer<IFinishedRecipe> consumer, BlockSet blockSet) {
+		private void registerBlockSetRecipes(Consumer<IFinishedRecipe> consumer, BlockSet blockSet) {
+			boolean hasIngot = false;
+			
+			if(hasIngot) {
+				
+			}
+			
+			//block
+			registerRecipe(consumer, blockSet.variants.get(0).get(), blockSet.baseBlock, Pattern.BLOCK, 3);
+			registerRecipe(consumer, blockSet.variants.get(1).get(), blockSet.baseBlock, Pattern.BLOCK, 2);
+			registerRecipe(consumer, blockSet.variants.get(2).get(), blockSet.baseBlock, Pattern.BLOCK, 2);
+			//stair
+			registerRecipe(consumer, blockSet.baseBlock, blockSet.variants.get(0).get(), Pattern.STAIR, 4);
+			registerRecipe(consumer, blockSet.variants.get(1).get(), blockSet.variants.get(0).get(), Pattern.STAIR, 2);
+			registerRecipe(consumer, blockSet.variants.get(2).get(), blockSet.variants.get(0).get(), Pattern.STAIR, 2);
+			//slab
+			registerRecipe(consumer, blockSet.baseBlock, blockSet.variants.get(1).get(), Pattern.SLAB, 4);
+			registerRecipe(consumer, blockSet.variants.get(0).get(), blockSet.variants.get(1).get(), Pattern.SLAB, 3);
+			registerRecipe(consumer, blockSet.variants.get(2).get(), blockSet.variants.get(1).get(), Pattern.SLAB, 2);
+			//wall
+			registerRecipe(consumer, blockSet.baseBlock, blockSet.variants.get(2).get(), Pattern.WALL, 4);
+			registerRecipe(consumer, blockSet.variants.get(0).get(), blockSet.variants.get(2).get(), Pattern.WALL, 3);
+			registerRecipe(consumer, blockSet.variants.get(1).get(), blockSet.variants.get(2).get(), Pattern.WALL, 2);
+			
 //			String id = blockSet.name.toLowerCase();
-//			//block recipe
+			//block recipe
 //			ConditionalRecipe.builder()
 //				.addCondition(modLoaded(MODID))
 //				.addRecipe(
@@ -65,5 +94,41 @@ public class Recipes extends RecipeProvider implements IConditionBuilder {
 //								.withRewards(AdvancementRewards.Builder.recipe(ID))
 //								.withCriterion("has_dirt", hasItem(Blocks.DIRT))))
 //				.build(consumer, new ResourceLocation("minecraft", id + "_block"));
-//		}
+		}
+		
+		private void registerRecipe(Consumer<IFinishedRecipe> consumer, Block ingredient, Block result, Pattern type, int value) {
+			ResourceLocation id = new ResourceLocation(ServerMod.MODID, ingredient.getRegistryName().getPath()+"_to_"+ result.getRegistryName().getPath());
+			
+			ShapedRecipeBuilder shapedRecipe = ShapedRecipeBuilder.shapedRecipe(result, value);
+			
+			switch(type) {
+				case BLOCK:
+					shapedRecipe = shapedRecipe.patternLine("XX").patternLine("XX");
+					break;
+				case STAIR:
+					shapedRecipe = shapedRecipe.patternLine("X").patternLine("XX");
+					break;
+				case SLAB:
+					shapedRecipe = shapedRecipe.patternLine("XX");
+					break;
+				case WALL:
+					shapedRecipe = shapedRecipe.patternLine("X").patternLine("X");
+					break;
+				case INGOT:
+					shapedRecipe = shapedRecipe.patternLine("X");
+					break;
+			}
+			shapedRecipe = shapedRecipe.key('X', ingredient)
+			.setGroup(result.getRegistryName().getPath())
+			.addCriterion("has_" + ingredient.getRegistryName().getPath(), hasItem(ingredient.asItem()));
+		}
+		
+		private enum Pattern {
+			BLOCK, STAIR, SLAB, WALL, INGOT;
+		}
+		
+		
+		
+		
+		
 	}
